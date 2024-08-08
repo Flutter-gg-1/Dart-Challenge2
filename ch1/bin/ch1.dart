15518d8dc770fa3b5d2f7e2950f97b1b215acef7
MAHA Al-ATAWI <mem880.a@gmail.com>
2024-08-08 12:09:31 +0300

    Error handeler

--- ch1/bin/ch1.dart
+++ ch1/bin/ch1.dart
@@ -1,19 +1,29 @@
+import 'dart:io';
+
 class BankAccount {
   double balance = 0;
-  void deposit(double amount) {
+  double deposit(double amount) {
     balance += amount;
+    return balance;
   }
 
-  void withdraw(double amount) {
-    balance -= amount;
+  double withdraw(double amount) {
+    balance += amount;
+    return balance;
   }
 }
 
 void main(List<String> args) {
-  var account = BankAccount();
-  account.deposit(100);
-  print(account.balance);
-  account.withdraw(50);
-  print(account.balance);
-  print(account.balance);
+  int input = int.parse(stdin.readLineSync()!);
+  try {
+    if (input < 0) {
+      throw Exception('invalid input');
+    }
+    print('Input: $input');
+    BankAccount account = BankAccount();
+    print('Deposit: ${account.deposit(input.toDouble())}');
+    print('Withdraw: ${account.withdraw(input.toDouble())}');
+  } catch (e) {
+    print('Error: $e');
+  }
 }
