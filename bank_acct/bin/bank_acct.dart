f81668e4e5ba64a95dc674443d2b7a5760e4588e
amer266030 <amer266030@users.noreply.github.com>
2024-08-08 11:57:52 +0300

    completed

---
+++ bank_acct/bin/bank_acct.dart
@@ -0,0 +1,30 @@
+void main(List<String> arguments) {
+  var mgr = BankMgr();
+
+  mgr.addMoney(amount: 100);
+  mgr.addMoney(amount: 400);
+  mgr.withdrawMoney(amount: 55);
+
+  mgr.showCurrentBalance();
+
+  mgr.showHistory();
+}
+
+class BankMgr {
+  double balance = 0;
+  List<Map<String, double>> history = [];
+
+  void addMoney({required double amount}) {
+    balance += amount;
+    history.add({'Deposit': amount});
+  }
+
+  void withdrawMoney({required double amount}) {
+    balance -= amount;
+    history.add({'Withdraw': amount});
+  }
+
+  void showCurrentBalance() => print(balance);
+
+  void showHistory() => print(history);
+}
---
+++ bank_acct/test/bank_acct_test.dart
@@ -0,0 +1,8 @@
+import 'package:bank_acct/bank_acct.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
