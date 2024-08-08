08debd6299ed5555b461ba31df3f89986d7e9cb0
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-08-08 11:59:55 +0300

    challenge 1

--- challenge2/bin/challenge2.dart
+++ challenge2/bin/challenge2.dart
@@ -1,23 +1,30 @@
-
 void main(List<String> arguments) {
-  
+
+
 }
 
 class BankAccount {
-
   double balance;
-  
+  static List? transaction;
+
   BankAccount({
     required this.balance,
   });
 
-deposit(double amount){
-balance+=amount;
-}
-withdraw(double amount){
-balance-=amount;
-}
-getBalance(){
-  print(balance);
-}
+  deposit(double amount) {
+    balance += amount;
+    
+  }
+
+  withdraw(double amount) {
+    balance -= amount;
+  }
+
+  getBalance() {
+    print(balance);
+  }
+
+  storeTransaction(){
+
+  }
 }
